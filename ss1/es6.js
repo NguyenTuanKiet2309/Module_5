8a5cf9b12e1d5fd3a8be1c516355bcf5275b2f76
Nguyen Tuan Kiet <github.com>
2023-07-17 11:22:40 +0700

    nop bai tap ss1

--- ss1/es6.js
+++ ss1/es6.js
@@ -1,55 +1,55 @@
-let courses = [ 
-    { 
-      id: 1, 
-      title: "ReactJS Tutorial", 
-      rating: 4.2, 
-    }, 
-    { 
-      id: 2, 
-      title: "Angular Tutorial", 
-      rating: 2.5, 
-    }, 
-    { 
-      id: 3, 
-      title: "VueJS Tutorial", 
-      rating: 3.8, 
-    }, 
-    { 
-      id: 4, 
-      title: "Java Tutorial", 
-      rating: 4, 
-    }, 
-    { 
-      id: 5, 
-      title: "JavaScript Tutorial", 
-      rating: 3.5, 
-    }, 
-  ];	
+let courses = [
+  {
+    id: 1,
+    title: "ReactJS Tutorial",
+    rating: 4.2,
+  },
+  {
+    id: 2,
+    title: "Angular Tutorial",
+    rating: 2.5,
+  },
+  {
+    id: 3,
+    title: "VueJS Tutorial",
+    rating: 3.8,
+  },
+  {
+    id: 4,
+    title: "Java Tutorial",
+    rating: 4,
+  },
+  {
+    id: 5,
+    title: "JavaScript Tutorial",
+    rating: 3.5,
+  },
+];
 
-  const rateCourses = courses.filter(courses => courses.rating>=4);
-  console.log(rateCourses);
+const rateCourses = courses.filter(courses => courses.rating >= 4);
+console.log(rateCourses);
 
-  const rateCourses1 = courses.filter(courses => courses.rating<4);
-  rateCourses1.forEach(courses => console.log(courses.id + '-' + courses.title + '-' + courses.rating));
+const rateCourses1 = courses.filter(courses => courses.rating < 4);
+rateCourses1.forEach(courses => console.log(courses.id + '-' + courses.title + '-' + courses.rating));
 
-  let addedCourses = [ 
-    { 
-      id: 6, 
-      title: "PHP Tutorial", 
-      rating: 3, 
-    }, 
-    { 
-      id: 7,
-      title: "C# Tutorial", 
-      rating: 2, 
-    }, 
-    { 
-      id: 8, 
-      title: "Docker Tutorial", 
-      rating: 3.8, 
-    } 
-  ]; 
+let addedCourses = [
+  {
+    id: 6,
+    title: "PHP Tutorial",
+    rating: 3,
+  },
+  {
+    id: 7,
+    title: "C# Tutorial",
+    rating: 2,
+  },
+  {
+    id: 8,
+    title: "Docker Tutorial",
+    rating: 3.8,
+  }
+];
 
-  const mergeCourses = [...courses,...addedCourses];
-  console.log(mergeCourses);
+const mergeCourses = [...courses, ...addedCourses];
+console.log(mergeCourses);
 
