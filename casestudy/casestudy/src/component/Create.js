5b7a01ec5342a240ca908c941fe19bf1522cfd1c
Nguyen Tuan Kiet <github.com>
2023-07-18 20:37:49 +0700

    casestudy

---
+++ casestudy/casestudy/src/component/Create.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+
+class Create extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create New Service </h1>
+        <div class="booking-form-w3layouts">
+          <form action="#" method="post">
+            <h2 class="sub-heading-agileits" style="color: #0091cd">
+              Furama's Service
+            </h2>
+            <div class="main-flex-w3ls-sectns">
+              <div class="field-agileinfo-spc form-w3-agile-text">
+                <input
+                  type="email"
+                  name="Email"
+                  placeholder="Email"
+                  required=""
+                />
+              </div>
+            </div>
+            <div class="main-flex-w3ls-sectns">
+              <div class="field-agileinfo-spc form-w3-agile-text">
+                <input
+                  type="email"
+                  name="Email"
+                  placeholder="Usage Area(m2)"
+                  required=""
+                />
+              </div>
+            </div>
+            <div class="main-flex-w3ls-sectns">
+              <div class="field-agileinfo-spc form-w3-agile-text">
+                <input
+                  type="email"
+                  name="Email"
+                  placeholder="Rental Cost($)"
+                  required=""
+                />
+              </div>
+            </div>
+
+            <div class="main-flex-w3ls-sectns">
+              <div class="field-agileinfo-spc form-w3-agile-text">
+                <input
+                  type="email"
+                  name="Email"
+                  placeholder="Capacity(People)"
+                  required=""
+                />
+              </div>
+            </div>
+            <div class="radio-section">
+              <h6>Type Of Rent</h6>
+              <ul class="radio-buttons-w3-agileits">
+                <li>
+                  <input type="radio" id="a-option" name="selector1" />
+                  <label for="a-option">Day</label>
+                  <div class="check"></div>
+                </li>
+                <li>
+                  <input type="radio" id="b-option" name="selector1" />
+                  <label for="b-option">Month</label>
+                  <div class="check">
+                    <div class="inside"></div>
+                  </div>
+                </li>
+                <li>
+                  <input type="radio" id="c-option" name="selector1" />
+                  <label for="c-option">Year</label>
+                  <div class="check">
+                    <div class="inside"></div>
+                  </div>
+                </li>
+              </ul>
+              <div class="clear"></div>
+            </div>
+
+            <div class="clear"></div>
+            <input type="submit" value="Submit" />
+            <input type="reset" value="Clear Form" />
+            <div class="clear"></div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Create;
--- casestudy/casestudy/src/component/Header.js
+++ casestudy/casestudy/src/component/Header.js
@@ -47,17 +47,9 @@ class Header extends Component {
                     <ul class="nav navbar-nav menu_nav">
                       <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
                       <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-                      <li class="nav-item"><a class="nav-link" href="properties.html">Properties</a></li>
-                      <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
-                      <li class="nav-item submenu dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                        aria-expanded="false">Blog</a>
-                        <ul class="dropdown-menu">
-                          <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
-                          <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
-                        </ul>
-                      </li>
-                      <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                      <li class="nav-item"><a class="nav-link" href="properties.html">Villa</a></li>
+                      <li class="nav-item"><a class="nav-link" href="gallery.html">House</a></li>
+                      <li class="nav-item"><a class="nav-link" href="contact.html">Room</a></li>
                     </ul>
                   </div>
         
