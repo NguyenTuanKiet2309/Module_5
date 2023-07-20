c5d9959819918e1aa9015c20851dd1ed677f6dd0
Nguyen Tuan Kiet <github.com>
2023-07-20 20:43:24 +0700

    nop bai tap

--- ss4/counts/src/App.js
+++ ss4/counts/src/App.js
@@ -1,20 +1,28 @@
 import "./App.css";
 import React, { useState } from "react";
 
-function App(){
-  const [counter1,setCounter1]=useState(0);
-  const [counter2,setCounter2]=useState(0);
- 
-    return (
-      <div>
-      <p>Count:{counter1}</p>
-      <button onClick={()=>(setCounter1((previousCounter)=>previousCounter+1))}>Add1</button>
-      <p>Count2:{counter2}</p>
-      <button onClick={()=>{setCounter2((previousCounter)=>previousCounter+1)}}>Add2</button>
-   
-      </div>
-    );
-    }
+function App() {
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
 
+  return (
+    <div>
+      <p>Count:{counter1}</p>
+      <button
+        onClick={() => setCounter1((previousCounter) => previousCounter + 1)}
+      >
+        Add1
+      </button>
+      <p>Count2:{counter2}</p>
+      <button
+        onClick={() => {
+          setCounter2((previousCounter) => previousCounter + 1);
+        }}
+      >
+        Add2
+      </button>
+    </div>
+  );
+}
 
 export default App;
